3aea35a2e16eab2e6a72a435778f83469ad39249
leetudit-hue <leetudit@gmail.com>
2026-06-11 22:47:55 +0530

    Time: 3 ms (5.77%) | Memory: 42.8 MB (27.85%) - LeetSync

--- 4307-exactly-one-consecutive-set-bits-pair/exactly-one-consecutive-set-bits-pair.java
+++ 4307-exactly-one-consecutive-set-bits-pair/exactly-one-consecutive-set-bits-pair.java
@@ -1,19 +1,20 @@
 class Solution {
     public boolean consecutiveSetBits(int n) {
         String binary = Integer.toBinaryString(n);
-        StringBuilder temp = new StringBuilder();
         int count = 0;
+        Stack<Character>st = new Stack<>();
         for(int i = 0 ; i < binary.length() ; i++){
-            temp.append(binary.charAt(i));
-            String tem = temp.toString();
-            if(tem.contains("11")){
-                count++;
-                temp.delete(0,temp.length()-1);
+            if(binary.charAt(i)=='1'){
+                if(!st.isEmpty()){
+                    if(st.peek() == '1'){
+                        count++;
+                    }
+                }
             }
+            st.push(binary.charAt(i));
             if(count > 1) return false;
         }
         if(count == 1) return true;
         return false;
-        
     }
 }
