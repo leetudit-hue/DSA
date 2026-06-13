9c411b3c0ffe4ab23ce31a5826a1e8857e21a0b0
leetudit-hue <leetudit@gmail.com>
2026-06-13 15:22:45 +0530

    Time: 3 ms (29.98%) | Memory: 45.3 MB (88.50%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,39 +1,19 @@
+
 class Solution {
-    public static TreeNode build(int[] pre, int prel, int preh,
-        int[] in, int inl, int inh) {
-
-        if (prel > preh || inl > inh) {
-            return null;
-        }
-
-        TreeNode root = new TreeNode(pre[prel]);
-
+    public static TreeNode build(int[] preorder, int[] inorder , int prel , int preh , int inl , int inh){
+        if(prel > preh || inl > inh) return null;
+        TreeNode root = new TreeNode(preorder[prel]);
         int i = inl;
-        while (i <= inh && in[i] != pre[prel]) {
+        while(i < inorder.length && inorder[i]!=preorder[prel]){
             i++;
         }
-
-        int left = i - inl;
-
-        root.left = build(pre,
-                          prel + 1,
-                          prel + left,
-                          in,
-                          inl,
-                          i - 1);
-
-        root.right = build(pre,
-                           prel + left + 1,
-                           preh,
-                           in,
-                           i + 1,
-                           inh);
-
+        int l = i - inl;
+        root.left = build(preorder,inorder,prel+1,prel+l,inl,i-1);
+        root.right = build(preorder,inorder,prel+l+1,preh,i+1,inh);
         return root;
     }
-
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         int n = preorder.length;
-        return build(preorder, 0, n - 1, inorder, 0, n - 1);
+        return build(preorder,inorder,0,n-1,0,n-1);
     }
 }
