8eab437d967b6a040aa04d0e4b8c1c769fa67f8f
leetudit-hue <leetudit@gmail.com>
2026-06-25 15:29:54 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (87.39%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -1,33 +1,19 @@
 
 class Solution {
-    public ListNode mergeTwoLists(ListNode h1, ListNode h2) {
+    public static ListNode sort(ListNode h1 , ListNode h2){
         if(h1 == null) return h2;
         if(h2 == null) return h1;
-        ArrayList<Integer>arr = new ArrayList<>();
-        ListNode temp = h1;
-        while(temp!= null){
-            arr.add(temp.val);
-            temp = temp.next;
+        if(h1.val<=h2.val){
+            h1.next = sort(h1.next,h2);
+            return h1;
         }
-        temp = h2;
-        while(temp!= null){
-            arr.add(temp.val);
-            temp = temp.next;
+        else{
+            h2.next = sort(h1,h2.next);
+            return h2;
         }
-        int[]arrr = new int[arr.size()];
-        int idx = 0;
-        for(int n : arr){
-            arrr[idx++] = n;
-        }
-        Arrays.sort(arrr);
-        ListNode head = new ListNode(arrr[0]);
-        temp = head;
-        for(int i = 1; i < arrr.length ; i++){
-            ListNode next = new ListNode(arrr[i]);
-            temp.next = next;
-            temp = temp.next;
-        }
-        temp.next = null;
+    }
+    public ListNode mergeTwoLists(ListNode h1, ListNode h2) {
+        ListNode head = sort(h1,h2);
         return head;
     }
 }
