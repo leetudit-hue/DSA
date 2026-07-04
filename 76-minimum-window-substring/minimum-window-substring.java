7e1e11e86781e7bfe3e2dd28dd7c3d1e6ffc07a2
leetudit-hue <leetudit@gmail.com>
2026-07-04 11:30:15 +0530

    Time: 15 ms (59.99%) | Memory: 47 MB (53.63%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,37 +1,37 @@
 class Solution {
     public String minWindow(String s, String t) {
         int n = s.length();
-        if(n < t.length()) return "";
+        int m = t.length();
+        if(m > n) return "";
         HashMap<Character,Integer>map = new HashMap<>();
-        for(int i = 0 ; i < t.length() ; i++){
+        for(int i = 0 ; i < m ; i++){
             char ch = t.charAt(i);
             map.put(ch,map.getOrDefault(ch,0)+1);
         }
-        int i = 0;
-        int j = 0;
-        int st = 0;
-        int minWindowLength = Integer.MAX_VALUE;
-        int req = t.length();
+        int i = 0 , j = 0;
+        int st = -1;
+        int minL = Integer.MAX_VALUE;
+        int count = m;
         while(j < n){
             char ch = s.charAt(j);
-            if(map.containsKey(ch) && map.get(ch)>0){
-                req--;
+            if(map.containsKey(ch) && map.get(ch) > 0){
+                count--;
             }
             map.put(ch,map.getOrDefault(ch,0)-1);
-            while(req == 0){
-                if (j - i + 1 < minWindowLength) {
-                    minWindowLength = j - i + 1;
+            while(count == 0){
+                if(minL > j - i + 1){
+                    minL = j - i + 1;
                     st = i;
                 }
                 char left = s.charAt(i);
-                map.put(left,map.getOrDefault(left,0)+1);
+                map.put(left,map.get(left)+1);
                 if(map.get(left) > 0){
-                    req ++;
+                    count ++;
                 }
                 i++;
             }
             j++;
         }
-        return minWindowLength == Integer.MAX_VALUE ? "" : s.substring(st,minWindowLength+st);
+        return st == -1 ? "" : s.substring(st,st+minL);
     }
 }
